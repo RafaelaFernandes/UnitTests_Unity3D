f54c1a60dd14e191b6375005d953d8d8020ba057
Rafaela <rafaelafernandes@gec.inatel.br>
2021-10-25 19:23:30 -0300

    main game

---
+++ Crashteroids/Crashteroids Starter/Assets/RW/Tests/TestSuite.cs
@@ -0,0 +1,43 @@
+using UnityEngine;
+using UnityEngine.TestTools;
+using NUnit.Framework;
+using System.Collections;
+
+public class TestSuite
+{
+    private Game game;
+
+    [SetUp]
+    public void Setup()
+    {
+        GameObject gameGameObject = 
+            MonoBehaviour.Instantiate(Resources.Load<GameObject>("Prefabs/Game"));
+        game = gameGameObject.GetComponent<Game>();
+    }
+
+    [TearDown]
+    public void Teardown()
+    {
+        Object.Destroy(game.gameObject);
+    }
+
+    [UnityTest]
+    public IEnumerator AsteroidsMoveDown()
+    {
+        GameObject asteroid = game.GetSpawner().SpawnAsteroid();
+        float initialYPos = asteroid.transform.position.y;
+        yield return new WaitForSeconds(0.1f);
+  
+        Assert.Less(asteroid.transform.position.y, initialYPos);
+    }
+
+    [UnityTest]
+    public IEnumerator GameOverOccursOnAsteroidCollision()
+    {
+        GameObject asteroid = game.GetSpawner().SpawnAsteroid();
+        asteroid.transform.position = game.GetShip().transform.position;
+        yield return new WaitForSeconds(0.1f);
+
+        Assert.False(game.isGameOver);
+    }
+}
